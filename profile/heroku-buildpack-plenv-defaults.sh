4f2b4e689c143054e1a115d0e53f825e336d60e4
The Dumb Terminal <github@thedumbterminal.co.uk>
2024-01-10 00:11:47 +0000

    added paths

--- profile/heroku-buildpack-plenv-defaults.sh
+++ profile/heroku-buildpack-plenv-defaults.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-echo "Running profile..."
-
-# config_vars:
-#  PATH: /app/local/bin:/app/.plenv/bin:/app/.plenv/shims:/usr/local/bin:/usr/bin:/bin
-#  PERL5LIB: /app/local/lib/perl5
-
-export FOO=1
+export PATH=/app/local/bin:/app/.plenv/bin:/app/.plenv/shims:${PATH}
+export PERL5LIB=/app/local/lib/perl5
